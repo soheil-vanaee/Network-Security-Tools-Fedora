f1e8a85f2544738f927a605c38d00f451b2f9271
soheil vanaee <119595980+soheil-vanaee@users.noreply.github.com>
2023-06-23 17:56:10 +0330

    Add files via upload

---
+++ Network-Security-Tools-Fedora.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo dnf install -y wireshark
+echo "wireshark done!"
+
+sudo dnf install -y tcpdump
+echo "tcpdump done!"
+
+sudo dnf install -y nmap
+echo "nmap done!"
+
+sudo dnf install -y suricata
+echo "suricata done!"
+
+sudo dnf install -y aircrack-ng
+echo "aircrack-ng done!"
+
+sudo dnf install -y netcat
+echo "netcat done!"
+
+sudo dnf install hydra
+echo "hydra done!"
+
+sudo dnf install hping3
+echo "hping3 done!"
+
+sudo dnf install ettercap
+echo "ettercap done!"
+
+sudo dnf install bettercap
+echo "bettercap done!"
